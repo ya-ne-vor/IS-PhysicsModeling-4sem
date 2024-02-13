e71626309fafafe93a5dbb883fe24ad03f831f0c
AnnaGonos <gonosanna@mail.ru>
2024-02-14 00:08:27 +0300

    added project

---
+++ Lorentz/LorentzForce/LorentzForce.js
@@ -0,0 +1,48 @@
+function MyFunction(){
+    var AmountCharge = document.getElementById("AmountCharge").value;
+
+    var ChargeSign= document.getElementById("ChargeSign").value;
+
+    var Induction= document.getElementById("Induction").value;
+
+    var SpeedCharge= document.getElementById("SpeedCharge").value;
+
+    var AngleBetween= document.getElementById("AngleBetween").value;
+
+    var chargeSign = (ChargeSign === 'neg') ? -1 : 1;
+
+
+    var Radius = SpeedCharge/(AmountCharge * Induction * Math.cos(AngleBetween * Math.PI / 180)); // радиус движения частицы
+    var Deviation = SpeedCharge * ((1/30)/(SpeedCharge * Math.cos(AngleBetween * Math.PI / 180) / Radius)) * Math.sin(AngleBetween * Math.PI / 180);
+
+    var pointCount = 300;
+
+    var x = [];
+    var y = [];
+    var z = [];
+    var c = [];
+
+    for(var i = 0; i < pointCount; i++)
+    {
+        x.push(Radius * Math.cos(i * chargeSign / 30));
+        y.push(Radius * Math.sin(i * chargeSign / 30));
+        z.push(i * chargeSign * Deviation);
+        c.push(i * 10)
+    }
+
+    // использую библиотеку для 3d изуализации, придания линии цвета и т.д.
+    Plotly.newPlot("myDiv", [{
+        mode: 'lines',
+        type: 'scatter3d',
+        x: x,
+        y: y,
+        z: z,
+        opacity: 1,
+        line: {
+            width: 10,
+            color: c,
+            colorscale: 'Rainbow'}
+    }]);
+
+    return false;
+}
