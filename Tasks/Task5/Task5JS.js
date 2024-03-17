0738bf611b1f55680cc1dc3dedf3493f7a0a2692
AnnaGonos <gonosanna@mail.ru>
2024-03-17 20:54:56 +0300

    add Task 5

---
+++ Tasks/Task5/Task5JS.js
@@ -0,0 +1,87 @@
+function MyFunction(){
+// https://studfile.net/preview/8823644/page:7/
+    // https://studfile.net/preview/8823644/page:7/
+    // все формулы брала из сайта выше
+    let R = document.getElementById("R").value;
+    let L = document.getElementById("L").value;
+    let E = document.getElementById("E").value;
+
+
+    let t = [];
+    let Connect = [];
+    let Disconnect = [];
+
+    for(let i = 0; i < 100; i+=0.0001)
+    {
+        t.push(i);
+        Connect.push(E/R*(1-Math.exp(-R/L*i)));
+        Disconnect.push(E/R*Math.exp(-R/L*i));
+
+        if (E/R*Math.exp(-R/L*i) < 0.00001)
+            break;
+    }
+
+    let layout_1 = {
+        title: {
+            text: 'График I(t) при замыкании цепи',
+            style: {
+                color: '#fdfdfd',
+                fontFamily: "Arial, Helvetica, sans-serif",
+                fontWeight: 'lighter'
+            }
+        },
+        xaxis: {
+            title: 'Время t (c)'
+        },
+        yaxis: {
+            title: 'I(t), А'
+        }
+    };
+
+    let layout_2 = {
+        title: {
+            text: 'График I(t) при размыкании цепи',
+            style: {
+                color: '#fdfdfd',
+                fontFamily: "Arial, Helvetica, sans-serif",
+                fontWeight: 'lighter'
+            }
+        },
+        xaxis: {
+            title: 'Время t (c)'
+        },
+        yaxis: {
+            title: 'I(t), А'
+        }
+    };
+
+    Plotly.newPlot(
+        "myDiv_1",
+        [{
+            mode: 'lines',
+            type: 'scatter',
+            x: t,
+            y: Connect,
+            line: {
+                color: '#683aa4' // Цвет линии - фиолетовый
+            }
+        }],
+        layout_1
+    );
+
+    Plotly.newPlot(
+        "myDiv_2",
+        [{
+            mode: 'lines',
+            type: 'scatter',
+            x: t,
+            y: Disconnect,
+            line: {
+                color: '#683aa4' // Цвет линии - фиолетовый
+            }
+        }],
+        layout_2
+    );
+
+    return false;
+}
